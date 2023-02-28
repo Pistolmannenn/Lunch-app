c0ba58af9ecea06a2233e19eeb137237306791de
Rasmusflorin <rasmus.florin@elev.ga.ntig.se>
2023-02-28 15:25:05 +0100

    adding stamps to mainpage

--- frontend/app/src/main/java/com/example/lunch_app/FirstFragment.java
+++ frontend/app/src/main/java/com/example/lunch_app/FirstFragment.java
@@ -8,12 +8,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lunch_app.databinding.FragmentFirstBinding;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    String []data = {"Hello","Hii","Welcome"};
+    int counter = 0;
 
     @Override
     public View onCreateView(
@@ -29,11 +36,26 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        List<String> items = new LinkedList<>();
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        DemoAdapter adapter = new DemoAdapter(items);
+        recyclerView.setAdapter(adapter);
+
+        binding.Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                items.add(data[counter%3]);
+                counter++;
+                adapter.notifyItemInserted(items.size()-1);
+                if (items.size() > 10){
+                    counter = 0;
+                    for (int i = 10; i >= 0; i--) {
+                        items.remove(i);
+                        adapter.notifyItemRemoved(i);
+                    }
+                }
             }
         });
     }
