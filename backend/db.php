cc11ee98e7785cac9587ca2631eacce1dd07b4cd
Bisexual | (Kev) <kevin.johansson@elev.ga.ntig.se>
2023-03-09 08:29:49 +0100

    Current version

--- backend/db.php
+++ backend/db.php
@@ -8,7 +8,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db = "takeee";
+    $db = "lunch";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password,$db);
